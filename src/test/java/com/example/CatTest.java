3a7f664da27a5cf4c16b6ce4d1b399d6d5cce751
Nikita Kalinichenko <testernikita@yandex.by>
2023-01-06 12:16:50 +0300

    Fixed bug with stub for getFoodLionMeatListValues method

--- src/test/java/com/example/CatTest.java
+++ src/test/java/com/example/CatTest.java
@@ -1,13 +1,21 @@
 package com.example;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.*;
 
+@RunWith(MockitoJUnitRunner.class)
 public class CatTest {
+
+    @Mock
+    private Feline feline;
 
     @Test
     public void getSoundCatMeow() {
@@ -20,8 +28,9 @@ public class CatTest {
     }
 
     @Test
-    public void getFoodCatMeatListValues() throws Exception{
-        final Feline feline = new Feline();
+    public void getFoodCatMeatListValues() throws Exception {
+        Mockito.when(feline.eatMeat()).thenReturn(Arrays.asList("Животные", "Птицы", "Рыба"));
+
         final Cat cat = new Cat(feline);
         final List<String> expectedCatMeatList = Arrays.asList("Животные", "Птицы", "Рыба");
         final List<String> actualCatMeatList = cat.getFood();
